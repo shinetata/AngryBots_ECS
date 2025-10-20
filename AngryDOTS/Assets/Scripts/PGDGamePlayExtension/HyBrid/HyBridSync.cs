323507f440317b758b88233ec1095d377e46ceca
shinetata <shinetata@noreply.gitcode.com>
2025-10-20 15:17:57 +0800

    特性修饰名变更

--- AngryDOTS/Assets/Scripts/PGDGamePlayExtension/HyBrid/HyBridSync.cs
+++ AngryDOTS/Assets/Scripts/PGDGamePlayExtension/HyBrid/HyBridSync.cs
@@ -1,7 +1,7 @@
 using PGD;
 using UnityEngine;
 
-[PGDDisableAutoRegister]
+[DisableAutoRegister]
 public class HybridTransformSync : PGDSystem<GoLink, PGDLocalTransform>
 {
     protected override void OnUpdate()
